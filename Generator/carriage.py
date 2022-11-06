934241c4fb2c6b1c7cf47794de4b0ba700179958
Pestap <90579181+Pestap@users.noreply.github.com>
2022-11-06 18:52:48 +0100

    Pojemnosc wagonu

--- Generator/carriage.py
+++ Generator/carriage.py
@@ -1,8 +1,9 @@
 class Carriage:
-    def __init__(self, id, model, prod_date, vmax, mass, station):
+    def __init__(self, id, model, prod_date, capacity, vmax, mass, station):
         self.id = id
         self.model = model
         self.prod_date = prod_date
+        self.capacity = capacity
         self.vmax = vmax
         self.mass = mass
         self.station = station
